4ade1fc4df04f61c7a24afe2c279dfec33b5d3ff
h3rald <h3rald@h3rald.com>
2014-11-15 21:39:29 +0100

    Fixed bug in directory listings; now compiles with original asynchttpserver lib.

--- nimhttpd/nimhttpd.nim
+++ nimhttpd/nimhttpd.nim
@@ -1,4 +1,4 @@
-import asynchttpserver2, asyncdispatch, asyncnet, os, strutils, mimetypes, times, parseopt2
+import asynchttpserver, asyncdispatch, asyncnet, os, strutils, mimetypes, times, parseopt2
 from strtabs import PStringTable, newStringTable
 from htmlgen as hg import nil
 
@@ -20,7 +20,7 @@ let usage = appname & " v" & appversion & " - Tiny Web Server for Static Sites" 
 
 var port = TPort(8888)
 var address = ""
-var cwd = getCurrentDir()
+var www = getCurrentDir()
 var server = newAsyncHttpServer()
 var mimes = newMimeTypes()
 
@@ -28,81 +28,6 @@ type TNimHttpResponse* = tuple[
   code: THttpCode,
   content: string,
   headers: PStringTable]
-
-proc h_page(content: string, title=""): string =
-  var res = "<!DOCTYPE html>\n"
-  var footer = "<div id=\"footer\">" & appname & " Web Server v" & appversion & "</div>"
-  res = res & "<html>\n" & 
-    hg.head(hg.title(title), "<style type=\"text/css\">"&style&"</style>", "<meta charset=\"UTF-8\">") & 
-    hg.body(hg.h1(title), content, footer) & 
-    "</html>"
-  return res
-
-proc relativePath(path): string =
-  var relpath = path.replace(cwd, "").replace("\\", "/")
-  if not relpath.endsWith("/"):
-    relpath = relpath&"/"
-  return relpath
-
-proc relativeParent(path): string =
-  return expandFilename(path/"../").relativePath
-
-proc sendNotFound(path): TNimHttpResponse = 
-  var content = hg.p("The page you requested cannot be found.");
-  return (code: Http404, content: h_page(content, $Http404), headers: newStringTable())
-
-proc sendNotImplemented(path): TNimHttpResponse =
-  var content = hg.p("This server does not support the functionality required to fulfill the request.");
-  return (code: Http501, content: h_page(content, $Http501), headers: newStringTable())
-
-proc sendStaticFile(path): TNimHttpResponse =
-  let mimetype = mimes.getMimetype(path.splitFile.ext[1 .. -1])
-  var file = path.readFile
-  return (code: Http200, content: file, headers: {"Content-type": mimetype}.newStringTable)
-
-proc sendDirContents(path): TNimHttpResponse = 
-  var res: TNimHttpResponse
-  var files = newSeq[string](0)
-  if path != cwd and path != cwd&"/" and path != cwd&"\\":
-    files.add hg.li(class="i-back entypo", hg.a(href=path.relativeParent(), "..")) 
-  var title = "Index of " & path.relativePath()
-  for i in walkDir(path):
-    let name = i.path.extractFilename
-    let relpath = i.path.relativePath();
-    if name == "index.html" or name == "index.htm":
-      return sendStaticFile(i.path)
-    if i.path.existsDir:
-      files.add hg.li(class="i-folder entypo", hg.a(href=relpath, name)) 
-    else:
-      files.add hg.li(class="i-file entypo", hg.a(href=relpath, name)) 
-  res = (code: Http200, content: h_page(hg.ul(files.join("\n")), title), headers: newStringTable())
-  return res
-
-proc printReqInfo(req) =
-  echo getLocalTime(getTime()), " - ", req.hostname, " ", req.reqMethod, " ", "/", req.url.path
-
-proc handleHttpRequest(req: TRequest): PFuture[void]=
-  printReqInfo(req)
-  let path = cwd/req.url.path
-  var res: TNimHttpResponse 
-  if req.reqMethod != "GET":
-    res = sendNotImplemented(path)
-  elif path.existsDir:
-    res = sendDirContents(path)
-  elif path.existsFile:
-    res = sendStaticFile(path)
-  else:
-    res = sendNotFound(path)
-  return respond(req, res.code, res.content, res.headers)
-
-proc handleCtrlC() {.noconv.} =
-  echo "\nExiting..."
-  server.close()
-  quit()
-
-setControlCHook(handleCtrlC)
-
-######
 
 for kind, key, val in getopt():
   case kind
@@ -125,18 +50,95 @@ for kind, key, val in getopt():
           echo "Error: Invalid port: '", val, "'"
           echo "Running on default port instead."
   of cmdArgument:
-    var dir = cwd/key
+    var dir = www/key
     if dir.existsDir:
-      cwd = expandFilename dir
+      www = expandFilename dir
     else:
       echo "Error: Directory '"&dir&"' does not exist."
       quit(1)
   else: 
     discard
 
+let cwd = www
+
+proc h_page(content: string, title=""): string =
+  var res = "<!DOCTYPE html>\n"
+  var footer = "<div id=\"footer\">" & appname & " Web Server v" & appversion & "</div>"
+  res = res & "<html>\n" & 
+    hg.head(hg.title(title), "<style type=\"text/css\">"&style&"</style>", "<meta charset=\"UTF-8\">") & 
+    hg.body(hg.h1(title), content, footer) & 
+    "</html>"
+  return res
+
+proc relativePath(path): string =
+  var relpath = path.replace(cwd, "").replace("\\", "/")
+  if (not relpath.endsWith("/")) and (not path.existsFile):
+    relpath = relpath&"/"
+  return relpath
+
+proc relativeParent(path): string =
+  return expandFilename(path/"../").relativePath
+
+proc sendNotFound(path): TNimHttpResponse = 
+  var content = hg.p("The page you requested cannot be found.");
+  return (code: Http404, content: h_page(content, $Http404), headers: newStringTable())
+
+proc sendNotImplemented(path): TNimHttpResponse =
+  var content = hg.p("This server does not support the functionality required to fulfill the request.");
+  return (code: Http500, content: h_page(content, $Http500), headers: newStringTable())
+
+proc sendStaticFile(path): TNimHttpResponse =
+  let mimetype = mimes.getMimetype(path.splitFile.ext[1 .. -1])
+  var file = path.readFile
+  return (code: Http200, content: file, headers: {"Content-type": mimetype}.newStringTable)
+
+proc sendDirContents(path): TNimHttpResponse = 
+  var res: TNimHttpResponse
+  var files = newSeq[string](0)
+  if path != cwd and path != cwd&"/" and path != cwd&"\\":
+    files.add hg.li(class="i-back entypo", hg.a(href=path.relativeParent(), "..")) 
+  var title = "Index of " & path.relativePath()
+  for i in walkDir(path):
+    let name = i.path.extractFilename
+    let relpath = i.path.relativePath()
+    if name == "index.html" or name == "index.htm":
+      return sendStaticFile(i.path)
+    if i.path.existsDir:
+      files.add hg.li(class="i-folder entypo", hg.a(href=relpath, name)) 
+    else:
+      files.add hg.li(class="i-file entypo", hg.a(href=relpath, name)) 
+  res = (code: Http200, content: h_page(hg.ul(files.join("\n")), title), headers: newStringTable())
+  return res
+
 echo appname , " Web Server v", appversion, " started on port ", int(port), "." 
 echo "Serving directory ", cwd
 
+proc printReqInfo(req) =
+  echo getLocalTime(getTime()), " - ", req.hostname, " ", req.reqMethod, " ", req.url.path
+
+proc handleHttpRequest(req: TRequest): PFuture[void] {.closure, gcsafe.}=
+  printReqInfo(req)
+  let path = cwd/req.url.path
+  var res: TNimHttpResponse 
+  if req.reqMethod != "GET":
+    res = sendNotImplemented(path)
+  elif path.existsDir:
+    res = sendDirContents(path)
+  elif path.existsFile:
+    res = sendStaticFile(path)
+  else:
+    res = sendNotFound(path)
+  return respond(req, res.code, res.content, res.headers)
+
+proc handleCtrlC() {.noconv.} =
+  echo "\nExiting..."
+  server.close()
+  quit()
+
+setControlCHook(handleCtrlC)
+
+######
+
 while true:
-  server.serve(port, handleHttpRequest, address)
+  discard server.serve(port, handleHttpRequest, address)
   runForever()
